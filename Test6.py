ade48982225d1fe35fe7ab2b5965304f166b5e4b
UmeshChandra123 <72588179+UmeshChandra123@users.noreply.github.com>
2020-10-09 22:32:49 +0530

    Create Test6.py

---
+++ Test6.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+c = int(input("Enter the third number"))
+d = int(input("Enter the fourth number"))
+e = int(input("Enter the fifth number"))
+f = int(input("Enter the sixth number"))
+g = a+b+C+D+E+F
+Print(G)
